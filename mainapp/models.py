934f42a2637a41a292a77517e8bc96b9b35d6caf
MikeGer <mikeman@inbox.ru>
2021-04-27 16:09:37 +0300

    created new tables for DB and make migrations

--- mainapp/models.py
+++ mainapp/models.py
@@ -9,6 +9,6 @@ class Product(models.Model):
     name = models.CharField(max_length=256)
     image = models.ImageField(upload_to='products_images', blank=True)
     description = models.CharField(max_length=64, blank=True)
-    price = models.DateTimeField(max_digits=8, max_places=2, default=0)
+    price = models.DecimalField(max_digits=8,decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
