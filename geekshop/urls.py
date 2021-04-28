e2c5c5db0d7d22f9d8ff1d6379fe4936def2e63a
MikeGer <mikeman@inbox.ru>
2021-04-28 17:25:32 +0300

    added product images from DB

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 from mainapp.views import index, products, test_context
 
 urlpatterns = [
@@ -24,3 +25,5 @@ urlpatterns = [
     path('products/', products, name='products'),
     path('test_context/', test_context, name='test_context'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
