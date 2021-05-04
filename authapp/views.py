41bdc2cc4f3ba2adac1bd25efefe6be47618efbd
MikeGer <mikeman@inbox.ru>
2021-05-04 16:41:50 +0300

    added form

--- authapp/views.py
+++ authapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from authapp.forms import UserLoginForm
 
 # Create your views here.
 def login(request):
-    context = {'title': 'Geekshop - Авторизация'}
+    form = UserLoginForm()
+    context = {'title': 'Geekshop - Авторизация', 'form': form}
     return render(request, 'authapp/login.html', context)
