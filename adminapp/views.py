d5a19b51a77c200319418663bde128212881adb9
MikeGer <mikeman@inbox.ru>
2021-05-19 19:06:30 +0300

    added users_read and users_create

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from authapp.models import User
+from adminapp.forms import UserAdminRegisterForm
 
 # Create your views here.
 def index(request):
     return render(request, 'adminapp/admin.html')
+
+
+def admin_users_read(request):
+    context = {'users':User.objects.all()}
+    return render(request, 'adminapp/admin-users-read.html', context)
+
+
+def admin_users_create(request):
+    if request.method == 'POST':
+        form = UserAdminRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            # messages.success(request, 'Вы успешно зарегистрировались!')
+            return HttpResponseRedirect(reverse('admin_staff:admin_users_read'))
+        else:
+            print(form.errors)
+    else:
+        form = UserAdminRegisterForm()
+    context = {'form': form}
+    return render(request, 'adminapp/admin-users-create.html', context)
+
+
+
