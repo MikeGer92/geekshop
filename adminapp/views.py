cd2b2fbccca7600b23a1dc068bb64d27fc597e5b
MikeGer <mikeman@inbox.ru>
2021-05-19 20:04:23 +0300

    added users_update

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from authapp.models import User
-from adminapp.forms import UserAdminRegisterForm
+from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
 
 # Create your views here.
 def index(request):
@@ -15,7 +15,7 @@ def admin_users_read(request):
 
 def admin_users_create(request):
     if request.method == 'POST':
-        form = UserAdminRegisterForm(data=request.POST)
+        form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
         if form.is_valid():
             form.save()
             # messages.success(request, 'Вы успешно зарегистрировались!')
@@ -27,5 +27,15 @@ def admin_users_create(request):
     context = {'form': form}
     return render(request, 'adminapp/admin-users-create.html', context)
 
-
+def admin_users_update(request, user_id):
+    selected_user = User.objects.get(id=user_id)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(data=request.POST, files=request.FILES, instance=selected_user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admin_staff:admin_users_read'))
+    else:
+        form = UserAdminProfileForm(instance=selected_user)
+    context = {'form': form, 'selected_user': selected_user}
+    return render(request, 'adminapp/admin-users-update-delete.html', context)
 
